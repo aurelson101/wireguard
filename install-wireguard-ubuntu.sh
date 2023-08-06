9d50010ac454f0d32660c33852d7d38fe30b2d42
AurelSon101 <contact@aurelson.com>
2023-08-06 14:34:29 +0200

    Update selection subnet and port

--- install-wireguard-ubuntu.sh
+++ install-wireguard-ubuntu.sh
@@ -22,6 +22,14 @@ function checkVirt() {
 	fi
 }
 
+# Check if the system is running Debian or Ubuntu
+if [ -f /etc/debian_version ]; then
+    echo "The system is running Debian or Ubuntu."
+else
+    echo "The system is not running Debian or Ubuntu, stopping script."
+    exit 1
+fi
+
 # Detect primary Ethernet port
 ETH=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
 echo "Primary Ethernet port: $ETH"
@@ -42,22 +50,64 @@ echo "net.ipv6.conf.all.forwarding = 1" >> /etc/sysctl.conf
 sysctl -p
 
 # Generate WireGuard key
-wg genkey | tee privatekey | wg pubkey > publickey
+umask 077
+wg genkey | tee /etc/wireguard/privatekey | wg pubkey | tee /etc/wireguard/publickey
+privkey=$(cat /etc/wireguard/privatekey)
+pubkey=$(cat /etc/wireguard/pubblickey)
+
+# Create Subnet ipv4 and ipv6
+echo "Please choose an option:"
+echo "1. Manually enter IPv4 and IPv6 subnets"
+echo "2. Automatically generate IPv4 and IPv6 subnets"
+read choice
+
+if [ "$choice" == "1" ]; then
+    echo "Please enter the IPv4 subnet (e.g. 10.20.30.1/24):"
+    read ipv4_subnet
+    echo "Please enter the IPv6 subnet (e.g. fd86:1111:2222::1/64):"
+    read ipv6_subnet
+elif [ "$choice" == "2" ]; then
+    ipv4_subnet="10.10.10.1/24"
+    ipv6_subnet="fd86:ea04:1115::1/64"
+else
+    echo "Invalid choice, exiting."
+    exit 1
+fi
+
+echo - Show Subnet configuration
+echo $ipv4_subnet
+echo $ipv6_subnet
+
+# Select port udp wireguard
+echo "Please choose an option:"
+echo "1. Manually enter port wireguard"
+echo "2. Default port (e.g. 51820)"
+read choice
+
+if [ "$choice" == "1" ]; then
+    echo "Manually enter port wireguard (e.g. 1194):"
+    read portw
+elif [ "$choice" == "2" ]; then
+    portw="51820"
+else
+    echo "Invalid choice, exiting."
+    exit 1
+fi
+
+echo - Wireguard port is 
+echo $portw
 
 # Configure WireGuard
-echo "[Interface]
-PrivateKey = $(cat privatekey)
-Address = 10.0.0.1/24, fd86:ea04:1115::1/64
-ListenPort = 51820" > /etc/wireguard/wg0.conf
-
+tee /etc/wireguard/wg0.conf <<EOL
+[Interface]
+PrivateKey = $privkey
+Address = $ipv4_subnet, $ipv6_subnet
+ListenPort = $portw
 #exemple iptables conf if ufw don't work :
-#PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-#PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-
-# exemple public peer don't forget to edit it
-#[Peer]
-#PublicKey = <PEER_PUBLIC_KEY>
-#AllowedIPs = 10.0.0.2/32, fd86:ea04:1115::2/128
+#PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $ETH -j MASQUERADE
+#PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $ETH -j MASQUERADE
+SaveConfig = true
+EOL
 
 # Start WireGuard
 wg-quick up wg0
@@ -76,17 +126,17 @@ ufw allow 53/udp
 ufw allow 123/udp
 ufw allow http
 ufw allow https
-ufw allow 51820/udp # WireGuard
+ufw allow $portw/udp # WireGuard
 # Enable NAT for outgoing traffic on eth0
 sed -i '1s/^/*nat\n:POSTROUTING ACCEPT [0:0]\n/' /etc/ufw/before.rules
 echo "-A POSTROUTING -o $ETH -j MASQUERADE" >> /etc/ufw/before.rules
 echo "COMMIT" >> /etc/ufw/before.rules
 ufw reload
 
-
+#exemple conf route and ufw subnet local
 # Create IPv4 and IPv6 subnets
-ip route add 192.168.0.0/16 dev wg0
-ip -6 route add fd86:ea04:1115::/48 dev wg0
+#ip route add 192.168.0.0/16 dev wg0
+#ip -6 route add fd86:ea04:1115::/48 dev wg0
 
 # Allow access to subnet 192.168.0.0/16
-ufw allow from 192.168.0.0/16 to any port 22 proto tcp
+#ufw allow from 192.168.0.0/16 to any port 22 proto tcp
